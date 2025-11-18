b704502c08de7f8b744b01ba7ed4c51f7ec6af98
prosenjit-deb <prosenjit.frontend@gmail.com>
2025-11-18 16:29:17 +0530

    up

--- components/callingStep/CallMissed.js
+++ components/callingStep/CallMissed.js
@@ -15,7 +15,7 @@
               />
               <h2 class="text-lg font-bold text-white">Missed Video Call</h2>
             </div>
-      <!-- calling-info-section -->
+      <!-- calling-info-section tt-->
       <img src="https://new-stage.fansocial.app/wp-content/plugins/fansocial/dev/chimenew/assets/svgs/x-close-thin.svg" class="absolute h-[24px] w-[24px] top-1 right-1 cursor-pointer"/>
       <div
         class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-max flex flex-col justify-center items-center gap-4 pointer-events-none z-0"
