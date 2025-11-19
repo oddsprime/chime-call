cf05f4374dc6c4f8b3bf81a84e9879871013167b
harry-fansocial <harwinder@fansocial.app>
2025-11-19 11:11:46 +0530

    issues fixed https://app.clickup.com/t/86evjg4kz

--- components/controlPanelbuttons/vueComponenChatButton.js
+++ components/controlPanelbuttons/vueComponenChatButton.js
@@ -1,23 +1,38 @@
 // vueComponentBottomRightControls.js
 (function (global) {
-    function register(app) {
-      app.component('chat-button', {
-        props: {
-          toggleChat: { type: Function, required: false },
-          chimeCallSettings: { type: Object, required: false },
-        },
-        template: `
-            <div data-icon-style="toggle" @click="toggleChat"
-                  :class="['h-[68px] w-[68px] rounded-full flex items-center justify-center cursor-pointer transition-colors duration-300',
-                    chimeCallSettings?.callChatStatus ? 'bg-white/75' : 'bg-white/10'
-                  ]"
-                 aria-label="Toggle chat">
-                 <img v-if="chimeCallSettings.callChatStatus" src="https://new-stage.fansocial.app/wp-content/plugins/fansocial/dev/chimenew/assets/svgs/message-dots-circle-black.svg" class="w-8 h-8 black-chat-icon" alt="Chat">
-                 <img v-else src="https://new-stage.fansocial.app/wp-content/plugins/fansocial/dev/chimenew/assets/svgs/chat-icon.svg" class="w-8 h-8" alt="Chat">
-            </div>
-        `
-      });
-    }
-    global.registerChatButton = register;
-  })(window);
-  
+  function register(app) {
+    app.component('chat-button', {
+      props: {
+        toggleChat: { type: Function, required: false },
+        chimeCallSettings: { type: Object, required: false },
+      },
+      mounted() {
+        document.addEventListener('click', this.handleOutsideClick);
+      },
+      unmounted() {
+        document.removeEventListener('click', this.handleOutsideClick);
+      },
+      methods: {
+        handleOutsideClick(event) {
+          const chatSection = document.getElementById('chat-section');
+          if (chatSection && !chatSection.contains(event.target)) {
+            if (this.chimeCallSettings?.callChatStatus) {
+              this.toggleChat && this.toggleChat();
+            }
+          }
+        }
+      },
+      template: `
+          <div data-icon-style="toggle" @click.stop="toggleChat"
+                :class="['h-[68px] w-[68px] rounded-full flex items-center justify-center cursor-pointer transition-colors duration-300',
+                  chimeCallSettings?.callChatStatus ? 'bg-white/75' : 'bg-white/10'
+                ]"
+               aria-label="Toggle chat">
+               <img v-if="chimeCallSettings.callChatStatus" src="https://new-stage.fansocial.app/wp-content/plugins/fansocial/dev/chimenew/assets/svgs/message-dots-circle-black.svg" class="w-8 h-8 black-chat-icon" alt="Chat">
+               <img v-else src="https://new-stage.fansocial.app/wp-content/plugins/fansocial/dev/chimenew/assets/svgs/chat-icon.svg" class="w-8 h-8" alt="Chat">
+          </div>
+      `
+    });
+  }
+  global.registerChatButton = register;
+})(window);
